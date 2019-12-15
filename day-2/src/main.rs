c660a2f8814d25b6c82e000a026623345d1c69e4
Amar1729 <amar.paul16@gmail.com>
2019-12-15 03:28:41 -0500

    d2 p2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -4,39 +4,51 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let mut numbers: Vec<u32> =
+    let numbers: Vec<u32> =
         input
         .split(',').map(|s| s.trim())
         .filter(|s| !s.is_empty())
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut curr = 0;
-    loop {
-        // println!("{:?}", numbers);
+    for i in 0..100 {
+        for j in 0..100 {
+            let mut memory = numbers.clone();
+            memory[1] = i;
+            memory[2] = j;
 
-        calc(curr, &mut numbers);
-        curr += 4;
+            let output = calc(&mut memory);
+            // println!("{}", output);
+            if output == 19690720 {
+                println!("{}", 100 * memory[1] + memory[2]);
+            }
+        }
     }
 }
 
-fn calc(curr: usize, numbers: &mut [u32]) {
-    let get_pos = |pos: usize| -> u32 { numbers[numbers[pos] as usize] };
+fn calc(numbers: &mut [u32]) -> u32 {
+    let mut curr = 0;
+    let mut opcode = numbers[curr];
 
-    let opcode = numbers[curr];
-    match opcode {
-        1 => {
-            let x1 = get_pos(curr+1);
-            let x2 = get_pos(curr+2);
-            numbers[numbers[curr+3] as usize] = x1 + x2;
-        },
-        2 => {
-            let x1 = get_pos(curr+1);
-            let x2 = get_pos(curr+2);
-            numbers[numbers[curr+3] as usize] = x1 * x2;
-        },
-        _ => { end(opcode, numbers); },
+    while opcode != 99 {
+        match opcode {
+            1 => {
+                let x1 = numbers[numbers[curr+1] as usize];
+                let x2 = numbers[numbers[curr+2] as usize];
+                numbers[numbers[curr+3] as usize] = x1 + x2;
+            },
+            2 => {
+                let x1 = numbers[numbers[curr+1] as usize];
+                let x2 = numbers[numbers[curr+2] as usize];
+                numbers[numbers[curr+3] as usize] = x1 * x2;
+            },
+            _ => { end(opcode, numbers); },
+        }
+        curr += 4;
+        opcode = numbers[curr];
     }
+
+    return numbers[0];
 }
 
 fn end(opcode: u32, numbers: &[u32]) {
