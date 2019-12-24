78c86a2b84067685b2ea8bb7dc690b03a5948316
Amar1729 <amar.paul16@gmail.com>
2019-12-23 23:41:53 -0500

    d8 p2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -2,54 +2,60 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-use std::collections::HashMap;
+const LENGTH: usize = 25 * 6;
+
+struct Img {
+    pixels: [usize; LENGTH],
+}
+
+impl Img {
+    fn new() -> Img {
+        let px = [2usize; LENGTH];
+
+        Img {
+            pixels: px,
+        }
+    }
+
+    fn update(&mut self, px: &[u32]) {
+        for i in 0..LENGTH {
+            match &self.pixels[i] {
+                2 => {
+                    self.pixels[i] = px[i] as usize;
+                },
+                _ => (),
+            }
+        }
+    }
+}
 
 fn main() {
-
-    // given by problem descr
-    let length = 25 * 6;
 
     let f = File::open("input.txt").unwrap();
     let mut reader = BufReader::new(f);
 
-    // there's probably a better way (iterating over vectors?) to calculate these
-    let mut minimum = 0;
-    let mut result = 0;
+    let mut img = Img::new();
 
     loop {
-        let mut buf = vec![0u8; length];
+        let mut buf = vec![0u8; LENGTH];
 
         if let Err(_) = reader.read_exact(&mut buf) { break; }
 
-        let output = calc(&buf);
+        let px: Vec<u32> = buf.iter().map(|b| *b as char).map(|c| c.to_digit(10).unwrap()).collect();
 
-        if minimum == 0 { minimum = output.0; }
+        img.update(&px);
+    }
 
-        if output.0 > 0 && output.0 < minimum {
-            minimum = output.0;
-            result = output.1;
+    // println!("{:?}", img);
+
+    for c in 0..LENGTH {
+        if c % 25 == 0 { print!("\n"); }
+
+        if img.pixels[c] == 1 {
+            print!("1");
+        } else {
+            print!(" ");
         }
+        // print!("{}", img.pixels[c]);
     }
-
-    println!("{}", result);
-}
-
-fn calc(pixels: &[u8]) -> (usize, usize) {
-
-    let mut charmap = HashMap::new();
-
-    //charmap.entry('0').or_insert(0);
-    //charmap.entry('1').or_insert(0);
-    //charmap.entry('2').or_insert(0);
-
-    for c in pixels.iter().map(|b| *b as char) {
-    // for c in pixels.map(|b| *b as char) {
-        let count = charmap.entry(c).or_insert(0);
-        *count += 1;
-    }
-
-    (
-        *charmap.get(&'0').unwrap(),
-        charmap.get(&'1').unwrap() * charmap.get(&'2').unwrap(),
-    )
 }
